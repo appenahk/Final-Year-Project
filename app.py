f06af52661c5ac9101c3543cf2a5891810d27572
Kieta <kieta.appenah@gmail.com>
2018-04-03 21:32:56 +0100

    Editing and breaking things

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, json, request, session
-from flask.ext.mysql import MySQL
+from flask_mysql import MySQL
 from time import gmtime, strftime
 from werkzeug import generate_password_hash, check_password_hash
 
@@ -34,12 +34,102 @@ def showSignin():
 def showAddFile():
     return render_template('addFile.html')
 
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
+
 @app.route('/userHome')
 def userHome():
     if session.get('user'):
         return render_template('userHome.html')
     else:
         return render_template('error.html',error = 'Unauthorized Access')
+@app.route('/showDashboard')
+def showDashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/editFile', methods=['POST'])
+def editFile():
+    try:
+        if session.get('user'):
+            _user = session.get('user')
+            _title = request.form['title']
+            _description = request.form['description']
+            _file_id = request.form['id']
+            _isPrivate = request.form['isPrivate']           
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_editFile',(_title,_description,_file_id,_user,_isPrivate))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'status':'OK'})
+            else:
+                return json.dumps({'status':'ERROR'})
+    except Exception as e:
+        return json.dumps({'status':'Unauthorized access'})
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.route('/deleteFile',methods=['POST'])
+def deleteFile():
+    try:
+        if session.get('user'):
+            _id = request.form['id']
+            _user = session.get('user')
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_deleteFile',(_id,_user))
+            result = cursor.fetchall()
+
+            if len(result) is 0:
+                conn.commit()
+                return json.dumps({'status':'OK'})
+            else:
+                return json.dumps({'status':'An Error occured'})
+        else:
+            return render_template('error.html',error = 'Unauthorized Access')
+    except Exception as e:
+        return json.dumps({'status':str(e)})
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.route('/getFile',methods=['POST'])
+def getWish():
+    try:
+        if session.get('user'):
+            _user = session.get('user')
+           
+
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('sp_GetFileByUser',(_user))
+            
+            files = cursor.fetchall()        
+
+            response = []
+            files_dict = []
+            for file in files:
+                files_dict = {
+                        'Id': file[0],
+                        'Title': file[1],
+                        'Description': file[2],
+                        'Date': file[4]}
+                files_dict.append(files_dict)
+            response.append(files_dict)
+            
+            return json.dumps(response)
+        else:
+            return render_template('error.html', error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html', error = str(e))
 
 @app.route('/getAllFiles')
 def getAllFiles():
@@ -50,16 +140,13 @@ def getAllFiles():
             cursor = conn.cursor()
             cursor.callproc('sp_GetAllFiles')
             result = cursor.fetchall()
-        
-
-        
+              
             file_dict = []
             for file in result:
                 file_dict = {
                         'Id': file[0],
                         'Title': file[1],
-                        'Description': file[2],
-                        'FilePath': file[3]}
+                        'Description': file[2]}
                 file_dict.append(file_dict)       
 
            
@@ -69,10 +156,59 @@ def getAllFiles():
             return render_template('error.html', error = 'Unauthorized Access')
     except Exception as e:
         return render_template('error.html',error = str(e))
+
+
+@app.route('/getFileById',methods=['POST'])
+def getFileById():
+    try:
+        if session.get('user'):
+            
+            _id = request.form['id']
+            _user = session.get('user')
     
-@app.route('/showDashboard')
-def showDashboard():
-    return render_template('dashboard.html')
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_GetFileById',(_id,_user))
+            result = cursor.fetchall()
+
+            files = []
+            files.append({'Id':result[0][0],'Title':result[0][1],'Description':result[0][2],'Private':result[0][3]})
+
+            return json.dumps(files)
+        else:
+            return render_template('error.html', error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+
+@app.route('/saveFile',methods=['POST'])
+def saveFile():
+    try:
+        if session.get('user'):
+            _id = request.form['id']
+            _user = session.get('user')
+    
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_GetFileById',(_id))
+            result = cursor.fetchall()
+
+            newFile.copy(result)
+            cursor.callproc('sp_addFile',(newFile[0][1],newFile[0][2],_user,newFile[0][3],newFile[0][4]))
+
+            if len(data) is 0:
+                conn.commit()
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'An error occurred!')
+
+        else:
+            return render_template('error.html',error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        conn.close()
+            
 
 @app.route('/addFile',methods=['POST'])
 def addFile():
@@ -138,6 +274,7 @@ def validateLogin():
         cursor.close()
         con.close()
 
+@app.route('/signUp',methods=['POST','GET'])
 def signUp():
     try:
         _name = request.form['inputName']
